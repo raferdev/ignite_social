27db8b368f7eaf1f52a07aa13622ea571bc32c74
Rafael <rafinha_fern@hotmail.com>
2023-01-18 16:14:04 -0300

    feat: post content and header styles

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     <Sidebar/>
     <main>
       <Post/>
+      <Post/>
     </main>
   </div>
   </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -11,14 +11,14 @@ export function Post() {
                     <span>Software Developer</span>
                     </div>
                 </div>
-            </header>
             <time title="Publicado 18 de Janeiro de 2023" dateTime="2023-01-18 15:00:00">Publicado há 1h</time>
+            </header>
             <div className={styles.content}>
                 
                 <p>Fala galeraa 👋</p>
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p ><a href="">👉 jane.design/doctorcare</a></p>
-                <p ><a href="">#novoprojeto #nlw #rocketseat</a></p>
+                <p ><a href="">👉{" "} jane.design/doctorcare</a></p>
+                <p ><a href="">#novoprojeto</a> {" "} <a href="">#nlw</a> {" "} <a href="">#rocketseat</a></p>
             
             </div>        
         </article>
