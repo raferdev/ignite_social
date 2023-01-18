92ec28e1064cd0abe08cafbd2e03f90fc3350cdc
Rafael <rafinha_fern@hotmail.com>
2023-01-18 18:08:18 -0300

    feat: new comments html and styles

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -20,7 +20,14 @@ export function Post() {
                 <p ><a href="">👉{" "} jane.design/doctorcare</a></p>
                 <p ><a href="">#novoprojeto</a> {" "} <a href="">#nlw</a> {" "} <a href="">#rocketseat</a></p>
             
-            </div>        
+            </div>
+            <form className={styles.commentForm}>
+            <strong>Deixe seu feedback</strong>  
+            <textarea placeholder='Deixe um comentário.'/>
+           <footer>
+           <button type="submit">Publicar</button>
+           </footer>
+            </form>        
         </article>
     )
 }
