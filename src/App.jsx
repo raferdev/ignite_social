103567e6020fa00aeea7034564147d9fedd7d6a0
Rafael <rafinha_fern@hotmail.com>
2023-01-19 17:34:20 -0300

    feat: new posts attributes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,76 @@
-import './global.css'
-import styles from './App.module.css';
+import "./global.css";
+import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar.jsx";
 
-import { Header } from "./components/Header.jsx"
-import {Post} from "./components/Post.jsx"
+import { Header } from "./components/Header.jsx";
+import { Post } from "./components/Post.jsx";
 
-
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/raferdev.png",
+      name: "Rafael Fernandes",
+      role: "Software Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-01-19 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/raferdev.png",
+      name: "Rafael Fernandes",
+      role: "Software Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2023-01-19 20:00:00"),
+  },
+];
 
 function App() {
-
   return (
-  <div>
-  <Header/>
-  <div className={styles.wrapper}>
-
-    <Sidebar/>
-    <main>
-      <Post/>
-      <Post/>
-    </main>
-  </div>
-  </div>
-  )
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
