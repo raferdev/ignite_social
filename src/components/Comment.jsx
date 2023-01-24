9b656c85a2e3229b8c891c0af6e93b462ff2fcfb
Rafael <rafinha_fern@hotmail.com>
2023-01-24 03:09:09 -0300

    feat: like count

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,16 @@
 import styles from "./Comment.module.css";
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar.jsx";
+import { useState } from "react";
 
 export function Comment({content, onDeleteComment}) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
   }
   return (
     <div className={styles.comment}>
@@ -28,9 +34,11 @@ export function Comment({content, onDeleteComment}) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-          Aplaudir <span>20</span>
+          Aplaudir <span>
+            {likeCount}
+          </span>
           </button>
         </footer>
       </div>
